2413eed919ec09489072ebe438610e3097f0972f
SanjanaGB <sanjanagb.cs17@rvce.edu.in>
2020-10-23 19:31:51 +0530

    First 4 Lab pg

---
+++ Header.h
@@ -0,0 +1,12 @@
+void s1_main(int, char**);
+void s2_main(int, char**);
+void s3_main(int, char**);
+void s4_main(int, char**);
+void s5_main(int, char**);
+void s6_main(int, char**);
+void s7_main(int, char**);
+void LabPA_1_main(int, char**);
+void LabPA_2_main(int, char**);
+void LabPA_3_main(int, char**);
+int LabPA_4_main(int, char**);
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+
+
+int main(int argc, char** argv) {
+
+	LabPA_4_main(argc, argv);
+}
